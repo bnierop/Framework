122a0ffe827103eb5274ba5f318620eba80d0cc9
Jake Nierop <jakenierop@gmail.com>
2016-12-19 23:05:10 +0100

    post functionaliteit aan request.php toegevoegd.

--- request.php
+++ request.php
@@ -2,7 +2,7 @@
 
 class Request{
 
-    function get($key, $default = null){
+    function get($key, $default = null) {
 
         if( $this->hasGet($key))
             return $_GET[$key];
@@ -10,7 +10,21 @@ class Request{
         return $default;
     }
 
+    function post($key){
+
+        if( $this->hasPost($key))
+            return $_POST[$key];
+    }
+
     function hasGet($key){
+
         return isset($_GET[$key]);
     }
+
+
+    function hasPost($key){
+        // check if the key has a value
+        return isset($_POST[$key]) && !empty($_POST[$key]);
+    }
+
 }
